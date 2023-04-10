d0c12b2ab18fde96693d570ac6a1feef8099ee0b
AaryakPrasad <aaryakprasad@gmail.com>
2023-04-10 22:30:16 +0530

    Basic styles

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express()
 const path = require('path')
 const methodOverride = require('method-override')
 const Rental = require('./models/rentals')
+const ejsMate = require('ejs-mate')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.engine('ejs', ejsMate)
 app.listen(3000, () => {
     console.log('Listening on port 3000.')
 })
--- models/rentals.js
+++ models/rentals.js
@@ -3,7 +3,8 @@ const schema = mongoose.Schema;
 
 const RentalSchema = new schema({
     title: String,
-    price: String,
+    price: Number,
+    image: String,
     description: String,
     location: String
 });
